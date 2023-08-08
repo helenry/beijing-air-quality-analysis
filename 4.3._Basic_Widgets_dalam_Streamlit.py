382784ba07d3410a552e6111a25546bc2b66d51b
helenry <67642100+helenry@users.noreply.github.com>
2023-08-08 20:24:26 +0700

    Delete 4.3._Basic_Widgets_dalam_Streamlit.py

--- 4.3._Basic_Widgets_dalam_Streamlit.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-test = pd.read_csv('https://docs.google.com/spreadsheets/d/' + 
-                   '1Iq0E9w6BKHRpiYhHXhXCxUDFBU6wm_dZ2kaXlZuSBPc' +
-                   '/export?gid=1428763389&format=csv',
-                   index_col=0
-                  )
-st.dataframe(test)
