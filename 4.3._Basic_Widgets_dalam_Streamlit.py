151e4297d0a9143e86f7ef0b33eecbc625ec0888
helenry <67642100+helenry@users.noreply.github.com>
2023-08-08 19:52:54 +0700

    Update 4.3._Basic_Widgets_dalam_Streamlit.py

--- 4.3._Basic_Widgets_dalam_Streamlit.py
+++ 4.3._Basic_Widgets_dalam_Streamlit.py
@@ -2,6 +2,13 @@ import streamlit as st
 import datetime
 import pandas as pd
 
+test = pd.read_csv('https://docs.google.com/spreadsheets/d/' + 
+                   '1Iq0E9w6BKHRpiYhHXhXCxUDFBU6wm_dZ2kaXlZuSBPc' +
+                   '/export?gid=1428763389&format=csv',
+                   index_col=0
+                  )
+st.dataframe(test)
+
 # Input Widget
 # .text_input()
 name = st.text_input(label="Nama lengkap", value="")
